8e1b4847a1d019f9075ae43c8d3a98f0beaf5345
Тимур <tima18152@gmail.com>
2024-09-10 11:38:26 +0300

    feat: Задание 2 Добавлены примеры функций с использованием различных флагов

---
+++ Laba_1/show_images.py
@@ -0,0 +1,34 @@
+import cv2
+
+
+def normal_image_png():
+    image_path = r'./images/image.png'
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def gray_image_jpg():
+    image_path = r'./images/image.jpg'
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    cv2.namedWindow('Image', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def any_image_jpeg():
+    image_path = r'./images/image.bmp'
+    image = cv2.imread(image_path, cv2.IMREAD_ANYCOLOR)
+    cv2.namedWindow('Image', cv2.WINDOW_FULLSCREEN)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    normal_image_png()
+    gray_image_jpg()
+    any_image_jpeg()
