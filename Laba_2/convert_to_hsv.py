8920ba11e3143d795f11eb72dda37083c606d03b
Тимур <tima18152@gmail.com>
2024-11-21 14:19:56 +0300

    feat: Лаба 2 задание 1

---
+++ Laba_2/convert_to_hsv.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+def convert_to_hsv():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        cv2.imshow('HSV', hsv)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    convert_to_hsv()
