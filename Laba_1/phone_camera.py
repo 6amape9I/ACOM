783225bf388e1274e7e6e6d296b8423b08d7310c
Тимур <tima18152@gmail.com>
2024-11-21 14:19:29 +0300

    feat: Задание 9

---
+++ Laba_1/phone_camera.py
@@ -0,0 +1,27 @@
+import cv2
+
+def phone_camera():
+    cap = cv2.VideoCapture(1)
+
+    if not cap.isOpened():
+        print("Error: Could not open camera.")
+        exit()
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Could not read frame.")
+            break
+
+        cv2.imshow('Camera', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    phone_camera()
