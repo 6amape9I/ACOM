ada398bcaa73a5ec4680a387508e1ce834030ed8
Тимур <tima18152@gmail.com>
2024-11-28 02:36:15 +0300

    feat: Лаба 3 задание 1

---
+++ Laba_3/fill_matrix.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def fill_matrix(core_size, standard_deviation):
+    core = np.ones((core_size, core_size))
+    a = b = (core_size + 1) // 2
+
+    for i in range(core_size):
+        for j in range(core_size):
+            core[i, j] = gauss(i, j, standard_deviation, a, b)
+
+    return core
+
+def gauss(x, y, omega, a, b):
+    omega2 = 2 * omega ** 2
+
+    m1 = 1 / (np.pi * omega2)
+    m2 = np.exp(-((x-a) ** 2 + (y-b) ** 2) / omega2)
+
+    return m1 * m2
+
+
+if __name__ == '__main__':
+
+    core_size = 3
+    standard_deviation = 10
+
+    core = fill_matrix(core_size, standard_deviation)
+
+    print(core)
+
