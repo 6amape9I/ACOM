284c759d66f68c733c1675563cdaba58bf1c8899
Тимур <tima18152@gmail.com>
2024-11-28 03:38:10 +0300

    feat: Лаба 3 Задание 4 Сравнение с cv

---
+++ Laba_3/compare_blur.py
@@ -0,0 +1,14 @@
+import cv2
+from Laba_3.gaus_blur import gaus_blur
+
+
+if __name__ == '__main__':
+    core_size = 5
+    standard_deviation = 10
+    img = cv2.imread(r'./images/image.png', cv2.IMREAD_GRAYSCALE)
+
+    blured_image = gaus_blur(img, core_size, standard_deviation)
+    cv2.imshow("Blured", blured_image)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
